bcaf2c6f881830d249c70974ab40dc0af4839031
Nygren <Kjell.Nygren@sanofi.com>
2020-06-11 13:44:37 -0400

    Replaced rnorm_gamma_reg with new version that does not depend on rmultireg and rwishart. Removed temporary versions of this file used during development of the new version. Used Dobsonlinreg example as starting point for Ex_rglmb_dispersion and Ex_rnorm_gamma_reg so that both functions are tested using examples tied to the respective functions.

--- R/rnorm_gamma_reg.R
+++ R/rnorm_gamma_reg.R
@@ -105,30 +105,125 @@ if (!all(ev >= -tol * abs(ev[1L])))
 
 famfunc=glmbfamfunc(gaussian())  
 f1=famfunc$f1
-    
-sim<-rmultireg(n=n,Y=matrix((y-offset2)*sqrt(wt),nrow=length(y)),X=x*sqrt(wt),Bbar=mu,A=P,nu=nu,V=V)
 
+## Convert as in call to rmultireg    
+
+Y=matrix((y-offset2)*sqrt(wt),nrow=length(y))
+X=x*sqrt(wt)
+Bbar=mu
+A=P
+nu=nu
+V=V
+
+#####    Beginning of rmultireg operations
+
+#
+# revision history:
+#    Modified 4/7/15 to produce produce vectorized output  
+#    changed 1/11/05 by P. Rossi to fix sum of squares error
+#
+# purpose:
+#    draw from posterior for Multivariate Regression Model with
+#    natural conjugate prior
+# arguments:
+#    Y is n x m matrix
+#    X is n x k
+#    Bbar is the prior mean of regression coefficients  (k x m)
+#    A is prior precision matrix
+#    nu, V are parameters for prior on Sigma
+# output:
+#    list of B, Sigma draws of matrix of coefficients and Sigma matrix
+# model:
+#    Y=XB+U  cov(u_i) = Sigma
+#    B is k x m matrix of coefficients
+# priors:  beta|Sigma  ~ N(betabar,Sigma (x) A^-1)
+#                   betabar=vec(Bbar)
+#                   beta = vec(B) 
+#          Sigma ~ IW(nu,V) or Sigma^-1 ~ W(nu, V^-1)
+
+l0=length(Y)
+Ytemp=matrix(Y,ncol=1)
+
+l1=nrow(Ytemp)
+if(l0>l1) stop("Dimensions of y not correct")
+
+m=1
+
+k=ncol(X)
+l2=nrow(X)
+
+if(l2!=l1) stop("Dimensions of X and Y are inconsistent")
+
+k1=dim(A)[1]
+k2=dim(A)[2]
+k3=length(Bbar)
+
+if(k1!=k) stop("dimensions of X and A are inconsistent")
+if(k2!=k) stop("dimensions of X and A are inconsistent")
+if(k3!=k) stop("dimensions of X and Bbar are inconsistent")
+
+
+#
+# first draw Sigma
+#
+RA=chol(A)
+W=rbind(X,RA)
+
+Z=rbind(Ytemp,matrix(RA%*%Bbar,ncol=1)) ## Force dimension to 1
+
+#   note:  Y,X,A,Bbar must be matrices!
+IR=backsolve(chol(crossprod(W)),diag(k))
+#                      W'W = R'R  &  (W'W)^-1 = IRIR'  -- this is the UL decomp!
+Btilde=crossprod(t(IR))%*%crossprod(W,matrix(Z,ncol=1))   
+#                      IRIR'(W'Z) = (X'X+A)^-1(X'Y + ABbar)
+S=crossprod(Z-W%*%Btilde)
+#                      E'E
+
+out1<-matrix(0,nrow=n,ncol=k)
+
+if(m==1){
+  
+  out2=matrix(0,nrow=n,ncol=1)  
+}
+
+if(m>1){
+  
+  out2<-vector("list", n)
+  
+}
+
+P_Post=P+t(X)%*%X
+mu_Post=solve(P_Post)%*%(mu+t(X)%*%Ytemp)
+
+a_prior=nu/2     ## Should be relationship to shape in Wishart  
+b_prior=V  ## Should be relationship to scale in Wishart
+
+a_post=a_prior+(nobs/2) # Posterior Shape parameter
+b_post=b_prior+0.5*S # Posterior rate
+
+out1<-matrix(0,nrow=n,ncol=k)
+dispersion=1/rgamma(n=n,shape=a_post,rate=b_post)
+out2<-matrix(dispersion,nrow=n,ncol=1)
+
+for(i in 1:n){out1[i,1:k]<- t(Btilde + IR%*%matrix(rnorm(m*k),ncol=m)*sqrt(dispersion[i])) }
 
 draws<-matrix(1,n)
 LL<-matrix(1,n)
 
 for(i in 1:n){
   
-    ## This function should return the negative log-likelihood 
+  ## This function should return the negative log-likelihood 
   
-    LL[i]=f1(b=sim$B[i,],y=y,x=x,alpha=offset2,wt=wt/sim$Sigma[i])	
+  LL[i]=f1(b=out1[i,],y=y,x=x,alpha=offset2,wt=wt/out2[i])	
 }
 
 
-#coefficients2=cbind(sim$B,sim$Sigma)
-
 outlist<-list(
-  coefficients=sim$B
-#  coefficients=coefficients2
-  ,PostMode=sim$BStar,
-              Prior=list(mean=as.numeric(mu),Precision=P),
-              iters=draws,famfunc=famfunc,Envelope=NULL,
-              dispersion=sim$Sigma,loglike=LL)
+  coefficients=out1
+  ,PostMode=Btilde,
+  Prior=list(mean=as.numeric(mu),Precision=P),
+  iters=draws,famfunc=famfunc,Envelope=NULL,
+  dispersion=dispersion,loglike=LL)
 
 colnames(outlist$coefficients)<-colnames(x)
 
@@ -138,4 +233,8 @@ class(outlist)<-c(outlist$class,"rglmb")
 
 return(outlist)
 
+#return(dispersion)
+
 }
+
+
