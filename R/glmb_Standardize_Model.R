afde95567538a6c268fd2b8e031dc861b6e0031a
Nygren <Kjell.Nygren@sanofi.com>
2020-06-12 18:29:18 -0400

    Switched glmb_Standardize_Model to roxygen2 style documentation and linked to R code example.

--- R/glmb_Standardize_Model.R
+++ R/glmb_Standardize_Model.R
@@ -1,3 +1,21 @@
+#' Standardize A Non-Gaussian Model
+#'
+#' Standardizes a Non-Gaussian Model prior to Envelope Creation
+#' @param y a vector of observations of length m
+#' @param x a design matrix of dimension m*p
+#' @param P a positive-definite symmetric matrix specifying the prior precision matrix of the variables.
+#' @param bstar a matrix containing the posterior mode from an optimization step
+#' @param A1 a matrix containing the posterior precision matrix at the posterior mode
+#' @return A list with the following components
+#' \item{bstar2}{Standardized Posterior Mode}
+#' \item{A}{Standardized Data Precision Matrix}
+#' \item{x2}{Standardized Design Matrix}
+#' \item{mu2}{Standardized Prior Mean vector}
+#' \item{P2}{Standardized Precision Matrix Added to log-likelihood}
+#' \item{L2Inv}{A matrix used when undoing the first step in standardization}
+#' \item{L3Inv}{A matrix used when undoing the second step in standardization}
+#' @example inst/examples/Ex_glmb_Standardize_Model.R
+
 
 glmb_Standardize_Model<-function(y, x, P, bstar, A1){
   
