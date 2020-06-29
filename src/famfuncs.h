2a6d015d78c86bfb604ff843ba699ccdf47c372b
Nygren <Kjell.Nygren@sanofi.com>
2020-06-29 11:37:07 -0400

    Added RSS function to the famfuncs.h header file.

--- src/famfuncs.h
+++ src/famfuncs.h
@@ -32,23 +32,5 @@ NumericVector dnorm_glmb( NumericVector x, NumericVector means, NumericVector sd
 NumericVector  f1_gaussian(NumericMatrix b,NumericVector y,NumericMatrix x,NumericVector alpha,NumericVector wt);
 NumericVector  f2_gaussian(NumericMatrix b,NumericVector y, NumericMatrix x,NumericMatrix mu,NumericMatrix P,NumericVector alpha,NumericVector wt);
 arma::mat  f3_gaussian(NumericMatrix b,NumericVector y, NumericMatrix x,NumericMatrix mu,NumericMatrix P,NumericVector alpha,NumericVector wt);
+NumericVector RSS(NumericVector y, NumericMatrix x,NumericMatrix b,NumericVector alpha,NumericVector wt);
 
-
-//List Set_Grid(Rcpp::NumericMatrix GIndex,  Rcpp::NumericMatrix cbars, Rcpp::NumericMatrix Lint);
-//List   setlogP(NumericMatrix logP,NumericVector NegLL,NumericMatrix cbars,NumericMatrix G3);
-
-//Rcpp::List Set_Grid_C(Rcpp::NumericMatrix GIndex,  Rcpp::NumericMatrix cbars, Rcpp::NumericMatrix Lint,Rcpp::NumericMatrix Down,Rcpp::NumericMatrix Up,Rcpp::NumericMatrix lglt,Rcpp::NumericMatrix lgrt,Rcpp::NumericMatrix lgct,Rcpp::NumericMatrix logU,Rcpp::NumericMatrix logP);
-//Rcpp::List   setlogP_C(NumericMatrix logP,NumericVector NegLL,NumericMatrix cbars,NumericMatrix G3,NumericMatrix LLconst);
-
-
-//void Set_Grid_C2(Rcpp::NumericMatrix GIndex,  Rcpp::NumericMatrix cbars, 
-//Rcpp::NumericMatrix Lint,
-//Rcpp::NumericMatrix Down,
-//Rcpp::NumericMatrix Up,
-//Rcpp::NumericMatrix lglt,
-//Rcpp::NumericMatrix lgrt,
-//Rcpp::NumericMatrix lgct,
-//Rcpp::NumericMatrix logU,
-//Rcpp::NumericMatrix logP);
-
-//void setlogP_C2(NumericMatrix logP,NumericVector NegLL,NumericMatrix cbars,NumericMatrix G3,NumericMatrix LLconst);
