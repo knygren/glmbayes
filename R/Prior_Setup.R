9036072cb05ba7ad1cec49b8c01bcb03d8e0485d
Nygren <Kjell.Nygren@sanofi.com>
2020-07-04 19:01:32 -0400

    Created a new version of Prior_Setup function that uses the formula instead of output from a model object to Set up the prior.

--- R/Prior_Setup.R
+++ R/Prior_Setup.R
@@ -2,6 +2,7 @@
 #'
 #' Sets up the structure for the Prior mean and Variance Matrices using information from a classical model.
 #' @param object a fitted model object of class \code{"glm"}. Typically the result of a call to \link{glm}.
+#' @inheritParams stats::model.frame
 #' @return A list with items related to the prior.
 #' \item{mu}{An initial version of the prior mean vector, populated with all zeros}
 #' \item{Sigma}{An initial version of the prior Variance-Covariance vector, populated as the diagonal identity matrix}
@@ -9,6 +10,8 @@
 #' \item{x}{The design matrix from \code{object} if it exists}
 #' @example inst/examples/Ex_Prior_Check.R
 #' @export
+#' @rdname Prior_Setup
+#' @order 1
 
 
 Prior_Setup<-function(object){
@@ -27,4 +30,29 @@ Prior_Setup<-function(object){
     
   }
 
-      
+#' @export
+#' @rdname Prior_Setup
+#' @order 2
+
+Prior_Setup_v2<-function(formula,data, subset = NULL, na.action = na.fail, 
+                         drop.unused.levels = FALSE, xlev = NULL, ...){
+  
+  
+  mf<-model.frame(formula)
+  x<-model.matrix(formula,mf)
+  
+  nvar=ncol(x)
+  var_names=colnames(x)
+  #nvar=length(object$coefficients)
+  mu=matrix(0,nrow=nvar,ncol=1)
+  Sigma=as.matrix(diag(nvar))
+  
+  rownames(mu)=var_names
+  colnames(mu)=c("mu")
+  rownames(Sigma)=var_names
+  colnames(Sigma)=var_names
+  
+  return(list(mu=mu,Sigma=Sigma,model=as.data.frame(object$model),x=as.matrix(object$x)))    
+  
+}
+
