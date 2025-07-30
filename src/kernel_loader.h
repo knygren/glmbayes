71a83a4fa24d8ab810dcb18b3b1b150f09686aed
knygren <kjell.a.nygren@gmail.com>
2025-07-30 13:28:52 -0400

    Wrap load_kernel_source and load_kernel_library header declarations to try to prevent undefined  symbol error in Linux during package loading

--- src/kernel_loader.h
+++ src/kernel_loader.h
@@ -4,8 +4,13 @@
 #pragma once
 #include <string>
 
+#ifdef USE_OPENCL
 std::string load_kernel_source(const std::string& relative_path, const std::string& package = "glmbayes");
+#endif
+
+#ifdef USE_OPENCL
 std::string load_kernel_library(const std::string& subdir, const std::string& package = "glmbayes");
+#endif
 
 
 #endif
